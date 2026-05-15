83ae37b7f7771a455b63017f2ea3fcb053a2a5bc
poojitha <goddetipoojitha@gmail.com>
2026-05-15 15:46:35 +0530

    made changes in rockpaperscissors

--- excercises/rockpaperscissors.py
+++ excercises/rockpaperscissors.py
@@ -1,21 +1,32 @@
 import random
-list=("rock","paper","scissors")
-comp=random.choice(list)
-user=input("enter choice (rock,paper,scissors):")
+list=("rock","paper","scissors","q")
+play=True
 
-print(f"player:{user}")
-print(f"computer:{comp}")
 
-if user==comp:
-    print("its a tie..")
-elif user=="rock" and comp=="scissors":
-    print("you won!!")
-elif user=="paper" and comp=="rock":
-    print("you won!!")
-elif user=="scissors" and comp=="paper":
-    print("you won!!")
-else:
-    print("computer won!! you lose...")
+while play:
+  comp=random.choice(list)
+  user=input("enter choice (rock,paper,scissors) q for quit:")
+  if user not in list:
+   print("enter correct option")
+  else:
+   if user=="q":
+     print("THANK YOU")
+     play= False
+     break
+   else:
+     print(f"player:{user}")
+     print(f"computer:{comp}")
+
+   if user==comp:
+     print("its a tie..")
+   elif user=="rock" and comp=="scissors":
+     print("you won!!")
+   elif user=="paper" and comp=="rock":
+     print("you won!!")
+   elif user=="scissors" and comp=="paper":
+     print("you won!!")
+   else:
+     print("computer won!! you lose...")
 
 
 
