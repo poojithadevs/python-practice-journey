6ed576d179f98fa0e6064bc40a1fcbf3da9b93cc
poojitha <goddetipoojitha@gmail.com>
2026-05-16 08:11:33 +0530

    functions basics

---
+++ functions.py
@@ -0,0 +1,14 @@
+def ar_rect(l,b):
+    print(f"length:{l}")
+    print(f"breadth:{b}")
+    print(f"area of rectangle={l*b}")
+
+
+ar_rect(5,6)
+ar_rect(52,76)
+ar_rect(51,106)
+ar_rect(51,46)
+ar_rect(52,16)
+
+
+
