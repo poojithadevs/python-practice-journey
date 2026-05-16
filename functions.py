7dcf014a3cfbb454409ad76fdb51a4e1f87cb558
poojitha <goddetipoojitha@gmail.com>
2026-05-16 08:21:38 +0530

    made modifications in functions

--- functions.py
+++ functions.py
@@ -2,13 +2,15 @@ def ar_rect(l,b):
     print(f"length:{l}")
     print(f"breadth:{b}")
     print(f"area of rectangle={l*b}")
+def perimeter(l,b):
+    return (2*(l+b))
 
 
 ar_rect(5,6)
-ar_rect(52,76)
-ar_rect(51,106)
-ar_rect(51,46)
-ar_rect(52,16)
+print(f"perimeter={perimeter(5,6)}")
+
+ar_rect(12,16)
+print(f"perimeter={perimeter(12,16)}")
 
 
 
