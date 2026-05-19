f29e7dc17dce92c398c3102c3492f3ab8a8eae2a
poojitha <goddetipoojitha@gmail.com>
2026-05-19 20:41:19 +0530

    added oop related program

---
+++ oop.py
@@ -0,0 +1,22 @@
+from oop1 import car;
+
+car1=car("BMW","alpina","white",2026,True)
+car2=car("audi","audi R8","red",2025,False)
+
+print("car1 details")
+print(car1.brand)
+print(car1.model)
+print(car1.color)
+print(car1.year)
+print(car1.for_sale)
+car1.drive()
+car1.manufacture()
+
+print("car2 details")
+print(car2.brand)
+print(car2.model)
+print(car2.color)
+print(car2.year)
+print(car2.for_sale)
+car2.drive()
+car2.manufacture()
---
+++ oop1.py
@@ -0,0 +1,13 @@
+class car:
+ def __init__(self,brand,model,color,year,for_sale):
+  self.brand=brand
+  self.model=model
+  self.color=color
+  self.year=year
+  self.for_sale=for_sale
+
+ def drive(self):
+  print(f"you can drive the {self.brand} {self.model}")
+
+ def manufacture(self):
+  print(f"the {self.brand} {self.model} is made in japan")
